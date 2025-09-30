1679e91799d0b16a7910b9a221de30ec83818c85
rstey <rstega780@gmail.com>
2025-09-30 16:53:44 +0300

    fixed the server.js

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,25 @@ import express from 'express';
 import authRouter from './src/router/auth.router.js';
 import dotenv from 'dotenv';
 import messeagaeRouter from './src/router/messageRouter.js';
-import path from 'path';
 
 dotenv.config();
 
-const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/auth', authRouter);
 app.use('/message', messeagaeRouter);
 
-try{
+try {
   // Serve static assets from frontend/dist
-  app.use(express.static(path.join(__dirname, './frontend/dist')));
+  app.use(express.static('./frontend/dist'));
   
   // Catch-all route for client-side routing
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
+    res.sendFile('./frontend/dist/index.html', { root: '.' });
   });
 }
-catch(err){
+catch(err) {
   console.error("Error serving static files:", err);
 }
 
